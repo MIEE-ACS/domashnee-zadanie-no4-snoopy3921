75b3f918b85cb2c155a2700428a1a308649b75be
Snoopymeoww <giahuy392001@gmail.com>
2022-11-30 00:50:25 +0300

    Add file code

---
+++ DZ4/4_1/Program.cs
@@ -0,0 +1,72 @@
+﻿static void Swap(ref int A, ref int B)
+{
+    int temp;
+    temp = A;
+    A = B;
+    B = temp;
+}
+
+
+
+try
+{
+    Console.WriteLine("User input N(array length): ");
+    int N = Convert.ToInt32(Console.ReadLine());
+
+    int[] Arr = new int[N];
+
+    Random rnd = new Random();
+
+    Console.Write("Array:\t\t\t\t");
+    for (int i = 0; i < N; i++)
+    {
+        Arr[i] = rnd.Next(-10, 10); //Value range [-10, 10]
+        Console.Write("{0}\t", Arr[i]);
+    }
+    int Min = Arr[0];
+    for (int i = 0; i < N; i++)
+    {
+        Min = Min < Arr[i] ? Min : Arr[i];
+    }
+    Console.WriteLine("\nMinimum of array:\t{0}", Min);
+    int Sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (Arr[i] != 0) continue;
+        else
+        {
+            for (int j = i; j < N; j++)
+            {
+                Sum += Arr[j];
+            }
+        }
+    }
+    Console.WriteLine("Sum of elements after the first zero number:\t{0}", Sum);
+    if (N % 2 == 0)
+    {
+        for (int i = 0; i < N; i += 2)
+        {
+            if (i >= N / 2) break;
+            Swap(ref Arr[i], ref Arr[N - 1 - i]);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < N; i += 2)
+        {
+            if (i >= (N - 1) / 2) break;
+            Swap(ref Arr[i], ref Arr[(N - 1) - 1 - i]);
+        }
+    }
+    Console.Write("New array after rearranging:\t");
+    for (int i = 0; i < N; i++)
+    {
+        Console.Write("{0}\t", Arr[i]);
+    }
+    Console.ReadKey();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    return;
+}
---
+++ DZ4/4_2/Program.cs
@@ -0,0 +1,37 @@
+﻿double[,] A = new double[10, 10], B = new double[10, 10];
+double s = 0;
+int n, cnt = 0;
+Random rand = new Random();
+Console.WriteLine("Original matrix:");
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        A[i, j] = rand.Next(-10, 11);
+        Console.Write(A[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("\nSmoothed matrix:");//Сглаженная матрица
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        n = 8; // Number of cells surrounding {i,j}cell
+        try { B[i, j] += A[i + 1, j]; }     catch { n--; }
+        try { B[i, j] += A[i + 1, j + 1]; } catch { n--; }
+        try { B[i, j] += A[i, j + 1]; }     catch { n--; }
+        try { B[i, j] += A[i - 1, j + 1]; } catch { n--; }
+        try { B[i, j] += A[i - 1, j]; }     catch { n--; }
+        try { B[i, j] += A[i - 1, j - 1]; } catch { n--; }
+        try { B[i, j] += A[i, j - 1]; }     catch { n--; }
+        try { B[i, j] += A[i + 1, j - 1]; } catch { n--; }
+        B[i, j] /= n;
+        if (i < j) s += Math.Abs(B[i, j]); // Sum of elements above main diagonal
+        if (B[i, j] > A[i, j]) cnt++; // Number of local minimum number in matrix A
+        Console.Write(Math.Round(B[i, j], 3) + "\t");
+    }
+    Console.WriteLine();
+}
+Console.Write("\nThe sum of elements above the main diagonal is equal to {0}", Math.Round(s, 3));
+Console.Write("\nNumber of local minimum number is equal to {0}", cnt);
